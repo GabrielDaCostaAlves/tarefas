7c1c6f3bbe4141f10ca389fbb7259c3c12547b3a
Gabriel Da Costa Alves <109757880+GabrielDaCostaAlves@users.noreply.github.com>
2025-09-13 00:52:30 -0300

    feat: adição do metodo para buscar por e-mail.

--- src/main/java/com/stayoff/tarefas/repository/UsuarioRepository.java
+++ src/main/java/com/stayoff/tarefas/repository/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package com.stayoff.tarefas.repository;
 
+import com.stayoff.tarefas.dto.entrada.UsuarioDto;
 import com.stayoff.tarefas.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+    Usuario findByEmail(String email);
 }
