ba0595590352b812a111bba1e1b409a630e972f5
Gabriel Da Costa Alves <109757880+GabrielDaCostaAlves@users.noreply.github.com>
2025-09-13 10:23:29 -0300

    fix: adição da validação de e-mail do validation

--- src/main/java/com/stayoff/tarefas/dto/entrada/UsuarioDto.java
+++ src/main/java/com/stayoff/tarefas/dto/entrada/UsuarioDto.java
@@ -1,5 +1,6 @@
 package com.stayoff.tarefas.dto.entrada;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -9,6 +10,7 @@ public record UsuarioDto(
         @Size(min = 3, max = 100, message = "O nome deve ter entre 3 e 100 caracteres.")
         String nome,
 
+        @Email
         @NotBlank(message = "O e-mail é obrigatório.")
         @Size(min = 5, max = 100, message = "O e-mail deve ter entre 5 e 100 caracteres.")
         String email,
