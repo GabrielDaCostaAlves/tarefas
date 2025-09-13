2cecae7c9e77edee16c900f1ecb01796d5f8fbf9
Gabriel Da Costa Alves <109757880+GabrielDaCostaAlves@users.noreply.github.com>
2025-09-12 21:51:05 -0300

    feat: criado os models do sistema.

---
+++ src/main/java/com/stayoff/tarefas/model/Tarefa.java
@@ -0,0 +1,40 @@
+package com.stayoff.tarefas.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String titulo;
+
+    @Lob
+    private String descricao;
+
+    @Column(nullable = false)
+    private Boolean concluido = false;
+
+    @Column(nullable = false)
+    private LocalDateTime dataCriacao = LocalDateTime.now();
+
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+}
---
+++ src/main/java/com/stayoff/tarefas/model/Usuario.java
@@ -0,0 +1,30 @@
+package com.stayoff.tarefas.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false,unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+
+}
