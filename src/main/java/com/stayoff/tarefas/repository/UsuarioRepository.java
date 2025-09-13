06eb4c1b5dfe63adfa3d3e0a1e64b37e4b150556
Gabriel Da Costa Alves <109757880+GabrielDaCostaAlves@users.noreply.github.com>
2025-09-12 22:03:29 -0300

    feat: criado os repositories do sistema.

---
+++ src/main/java/com/stayoff/tarefas/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.stayoff.tarefas.repository;
+
+import com.stayoff.tarefas.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+}
